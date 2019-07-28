cc30154013e013d088c5abe9ebe09d46ad7d8a3f
fahriddin23 <fahriddin24@gmail.com>
2019-07-28 16:54:25 -0500

    added more tags

--- instance.tf
+++ instance.tf
@@ -2,5 +2,15 @@
         ami = "ami-0b898040803850657"
         instance_type = "t2.micro"
         key_name = "terraform_april"
+
+
+
+
+        tags = {
+            Name = "Dev"
+            Dept = "IT"
+            Group = "April"
+            Created_by = "Frank"
+        }
     }
 
