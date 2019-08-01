0ed1b84808977d1b732bd028166b094376cd5544
fahriddin23 <fahriddin24@gmail.com>
2019-07-31 19:33:56 -0500

    added userdata

--- instance.tf
+++ instance.tf
@@ -3,6 +3,7 @@
         instance_type = "t2.micro"
         key_name = "${aws_key_pair.terraform_april.key_name}"
         security_groups = ["${aws_security_group.sec1.name}"]
+        user_data = "${file("user_data.sh")}"
 
 
 
