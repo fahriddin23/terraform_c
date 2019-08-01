6550c3bb3f69fc1cb32af7d3da44b1ff1e001394
fahriddin23 <fahriddin24@gmail.com>
2019-07-31 20:34:24 -0500

    added variables

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
         dynamodb_table = "dev_state_lock"
-        bucket = "terraform-april-2019-fahrid"
-        region = "us-east-1"
-        key = "dev.tfstate"
+        bucket  = "terraform-april-2019-fahrid"
+        region  = "us-east-1"
+        key     = "dev.tfstate"
     }
 }
